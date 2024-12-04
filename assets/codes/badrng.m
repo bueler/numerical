7fc89434df7cbb1049efb1797577c7d2f07f8729
Ed Bueler <elbueler@alaska.edu>
2024-12-03 23:06:47 -0800

    minor fiddle

--- assets/codes/badrng.m
+++ assets/codes/badrng.m
@@ -1,19 +1,22 @@
 function x = badrng(N)
 % BADRNG  Use a linear congruential generator to generate
 % an array of length N of pseudorandom numbers from a fixed seed.
-% Output x are intended to be uniform on [0,1].
-% WARNING: DO NOT USE FOR PRODUCTION SIMULATIONS.
+% Outputs in x are intended to be uniform on [0,1].
+%
+% This is merely a demonstration of how pseudorandom numbers might
+% be generated.  WARNING: DO NOT USE FOR PRODUCTION SIMULATIONS.
+%
 % Comparison to built-in RAND:
 %   >> xb = badrng(1e4);  xr = rand(1,1e4);
 %   >> subplot(1,2,1),  hist(xb),  title('badrng')
 %   >> subplot(1,2,2),  hist(xr),  title('rand')
 
-% numbers from Press et al 1986 Numerical Recipes;
+% magic numbers from Press et al 1986 Numerical Recipes etc.
 % see https://en.wikipedia.org/wiki/Linear_congruential_generator
 a = 1664525;
 c = 1013904223;
 m = 2^32;
-y = 1234;  % fixed seed
+y = 123456789;  % fixed seed
 x = zeros(1,N);
 for j = 1:N
     y = mod(a*y + c, m);
